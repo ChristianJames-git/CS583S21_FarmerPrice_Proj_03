19215df82a955faf1428d48e833ade24a4a8ec35
Samuel Schneider <sschneider2883.@sdsu.edu>
2021-04-23 12:10:43 -0700

    added waypoint system
    added a waypoint script to attach to the waypoint object serving as a path for enemies to follow

---
+++ Assets/Scripts/Enemy/Waypoints.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Waypoints : MonoBehaviour
+{
+    //static so objects can access it without a direct reference
+    public static Transform[] waypoints;
+
+    private void Awake()
+    {
+        waypoints = new Transform[transform.childCount];
+
+        //go through all the children of this object and add them to the array in order
+        for (int i = 0; i < waypoints.Length; i++)
+            waypoints[i] = transform.GetChild(i);
+    }
+}
