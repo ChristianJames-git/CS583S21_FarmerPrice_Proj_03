11a33bc99c4d55befee8158cdf08249455ce33de
Shane Tasker <stasker5003@sdsu.edu>
2021-04-28 11:07:32 -0700

    Added empty scenes for How To Play, You Won, and You Died. Added scene manager script

---
+++ Assets/Scripts/ScenesManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScenesManager : MonoBehaviour
+{
+
+
+    public void onPlayGameButtonClicked()
+    {
+        SceneManager.LoadScene("GameScene");
+
+    }
+
+    public void onMainMenuButtonClicked()
+    {
+
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void onHowToPlayButtonClicked()
+    {
+
+        SceneManager.LoadScene("HowToPlay");
+    }
+    public void onQuitClicked()
+    {
+        Debug.Log("On Quit Button Clicked.");
+
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+
+    }
+}
