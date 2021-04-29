c38293e23f6a40d95f59dc687392b7f22c8e55fc
Cole Kincaid <ckincaid6921@sdsu.edu>
2021-04-29 10:49:09 -0700

    added a MapManager GameObject and script
    - when m is pressed the camera is switched from first person to a top down view of the entire map

---
+++ Assets/MapManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapManager : MonoBehaviour
+{
+    public Camera topDownCam;
+    public Camera firstPersonCam;
+    bool inMap;
+
+    private void Start()
+    {
+        topDownCam.enabled = false;
+        firstPersonCam.enabled = true;
+        inMap = false;
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if(Input.GetKeyDown(KeyCode.M))
+        {
+            topDownCam.enabled = !topDownCam.enabled;
+            firstPersonCam.enabled = !firstPersonCam.enabled;
+            inMap = !inMap;
+        }
+    }
+}
