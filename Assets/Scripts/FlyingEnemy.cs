0b5620874cfaedf982a0b81e1b6881a9426598c5
Samuel Schneider <sschneider2883.@sdsu.edu>
2021-04-30 21:32:28 -0700

    fixed bug where flying enemy would not move

--- Assets/Scripts/FlyingEnemy.cs
+++ Assets/Scripts/FlyingEnemy.cs
@@ -29,27 +29,26 @@ public class FlyingEnemy : EnemyBase
 
         //direction vector points form this object to the target
         Vector3 dir = target - transform.position;
-        print(target.ToString());
 
 
         //move the enemy if not in the map
         if (GameObject.Find("MapManager").GetComponent<MapManager>().inMap == false)
         {
             transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+            //print(dir.ToString());
         }
     }
 
     protected new void FindWayPoint()
     {
         // check if this enemy has reached the target
-        if (Vector3.Distance(transform.position + (Vector3.down * flightHeight), target) <= 0.2f)//enemy is flying so it will always be above the waypoints
+        if (Vector3.Distance(transform.position, target) <= 0.2f)
         {
-            print("hello");
             //find the next target
             pointIndex++;
             if (Waypoints.points.Length > pointIndex)
             {
-                target = Waypoints.points[pointIndex].position + new Vector3(0, flightHeight, 0); ;
+                target = Waypoints.points[pointIndex].position + new Vector3(0, flightHeight, 0);
             }
                 
             else
