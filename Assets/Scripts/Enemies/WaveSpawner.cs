cfd92bab554a2c3f77c2b6195126ca81a32570cc
Samuel Schneider <sschneider2883.@sdsu.edu>
2021-05-01 18:36:41 -0700

    wrote the waveSpawner script

--- Assets/Scripts/Enemies/WaveSpawner.cs
+++ Assets/Scripts/Enemies/WaveSpawner.cs
@@ -10,11 +10,11 @@ public class WaveSpawner : MonoBehaviour
 
         private float spawnTimer;   //how often to spawn
         private float lifeTime; //how long to spawn for
-        private string enemy;   //what enemy to spawn
+        private GameObject enemy;   //what enemy to spawn
         private float timeToSpawn; //at what time in the level the enemy will spawn
 
         //constuctor
-        public SpawnContainer(float timeSpawn, float spawnClock, float timeAlive, string enemyType)
+        public SpawnContainer(float timeSpawn, float spawnClock, float timeAlive, GameObject enemyType)
         {
             timeToSpawn = timeSpawn;
             spawnTimer = spawnClock;
@@ -33,7 +33,7 @@ public class WaveSpawner : MonoBehaviour
             return lifeTime;
         }
 
-        public string GetEnemyType()
+        public GameObject GetEnemyType()
         {
             return enemy;
         }
@@ -45,12 +45,44 @@ public class WaveSpawner : MonoBehaviour
 
     }
 
-    public List<SpawnContainer> spawns;
+    public List<SpawnContainer> waves;
+    public GameObject spawner;
+    private float timer;
     public GameObject flyingEnemy;
     public GameObject groundEnemy;
 
     private void Awake()
     {
-        spawns = new List<SpawnContainer> { };
+        waves = new List<SpawnContainer> { };
+        //start timer at 0
+        timer = 0;
+    }
+
+    private void Update()
+    {
+        //set timer
+        timer += Time.deltaTime * Time.timeScale;
+
+        if (waves.Count > 0)
+        {
+            EnemySpawner temp;
+
+            //check is the next wave should spawn
+            if (waves[0].GetTimeToSpawn() >= timer)
+            {
+                //spawn that enemy spawner
+                temp = Transform.Instantiate(spawner, new Vector3(0, 0, 0), Quaternion.identity).GetComponent<EnemySpawner>();
+                temp.SetSettings(waves[0].getSpawnTimer(), waves[0].GetLifeTime(), waves[0].GetEnemyType());
+
+                //remove it form the list, we have spawned the wave
+                waves.RemoveAt(0);
+            }
+        }
+        else
+        {
+            //we dont have anymore waves to spawn
+            Destroy(this.gameObject);
+        }
+
     }
 }
