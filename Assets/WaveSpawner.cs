4a7d628a1e854d7e7c10853e61758fb8b7ab8e59
Samuel Schneider <sschneider2883.@sdsu.edu>
2021-05-01 18:06:48 -0700

    created rudimentary WaveSpawner

---
+++ Assets/WaveSpawner.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour
+{
+    //class for storing details about what to spawn
+    public class SpawnContainer
+    { 
+
+        private float spawnTimer;   //how often to spawn
+        private float lifeTime; //how long to spawn for
+        private string enemy;   //what enemy to spawn
+        private float timeToSpawn; //at what time in the level the enemy will spawn
+
+        //constuctor
+        public SpawnContainer(float timeSpawn, float spawnClock, float timeAlive, string enemyType)
+        {
+            timeToSpawn = timeSpawn;
+            spawnTimer = spawnClock;
+            lifeTime = timeAlive;
+            enemy = enemyType;
+        }
+
+        //getter methods
+        public float getSpawnTimer()
+        {
+            return spawnTimer;
+        }
+
+        public float GetLifeTime()
+        {
+            return lifeTime;
+        }
+
+        public string GetEnemyType()
+        {
+            return enemy;
+        }
+
+        public float GetTimeToSpawn()
+        {
+            return timeToSpawn;
+        }
+
+    }
+
+    public List<SpawnContainer> spawns;
+    public GameObject flyingEnemy;
+    public GameObject groundEnemy;
+
+    private void Awake()
+    {
+        spawns = new List<SpawnContainer> { };
+    }
+}
