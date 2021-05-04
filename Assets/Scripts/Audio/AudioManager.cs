fab79db4d3b93d1e9167fc4e63747b8ba9fe33e3
Samuel Schneider <sschneider2883.@sdsu.edu>
2021-05-04 12:20:18 -0700

    added an AudioManager to deal with  playing audio

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine.Audio;
+using System;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+
+    public static AudioManager _instance;
+
+    private void Awake()
+    {
+        if (_instance == null)
+            _instance = this;
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+
+        DontDestroyOnLoad(this.gameObject);
+
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.GetComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+    }
+
+    public void PlaySound(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound not Found");
+            return;
+        }
+        s.source.Play();
+
+    }
+
+}
