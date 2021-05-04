bdc2090556ec41f0a67649643229416fc8e0aaa3
Christian James <cjames6305@sdsu.edu>
2021-05-04 16:17:12 -0700

    Credits Scene

--- Assets/Scripts/Menus/CreditSceneManager.cs
+++ Assets/Scripts/Menus/CreditSceneManager.cs
@@ -1,16 +1,32 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class CreditSceneManager : MonoBehaviour
 {
+    public GameObject panel1, panel2, panel3;
+    private List<GameObject> panelList;
+    private int currPanel;
     // Start is called before the first frame update
     void Start()
     {
-        
+        panelList = new List<GameObject>() { panel1, panel2, panel3 };
+        ResetPanels();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void ResetPanels()
     {
-        
+        foreach (GameObject panel in panelList)
+            panel.SetActive(false);
+        currPanel = 0;
+        panelList[0].SetActive(true);
+    }
+
+    public void Next()
+    {
+        panelList[currPanel++].SetActive(false);
+        if (currPanel < panelList.Count)
+            panelList[currPanel].SetActive(true);
+        else
+            GameManager.Instance.ChangeScene("MainMenu");
     }
 }
