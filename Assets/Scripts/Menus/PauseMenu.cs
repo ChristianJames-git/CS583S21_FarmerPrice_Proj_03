fc63f6a29e77202f27f9766ff0c4de5dfcf153f5
Shane Tasker <stasker5003@sdsu.edu>
2021-05-04 10:37:11 -0700

    Added code for pause menu

---
+++ Assets/Scripts/Menus/PauseMenu.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public GameObject ui;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+
+
+        }
+    }
+
+    public void Toggle()
+    {
+        ui.SetActive(!ui.activeSelf);
+
+        if(ui.activeSelf)
+        {
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            Time.timeScale = 1f;
+        }
+
+    }
+
+    public void Retry()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
+    }
+
+    public void Exit()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }    
+}
